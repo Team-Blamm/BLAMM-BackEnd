dd9fa760ae7ea04c81fa442375d500f26c76554e
Brad Broge <brbroge@gmail.com>
2017-08-01 13:43:27 -0400

    adding static files to beginning routes

---
+++ README.md
@@ -0,0 +1,12 @@
+# BLAMM
+## Team BLAMM backend
+<hr>
+
+## Hosted on Heroku
+### https://blamm-store-backend.herokuapp.com/
+
+## API Documentation
+#### Get Endpoints:
+- /api/v1/products - Returns a list of all of the hero's and villain's in the DB
+- /api/v1/products/type/:type - Takes a title (hero, villain) and returns a list of only hero's of that type
+- /api/v1/products/name/:title - Takes a listings title and returns full details about that product (currently only works for Batman)
--- routes/pub.js
+++ routes/pub.js
@@ -7,6 +7,107 @@ const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 
 // const models = require('../models');
+router.get('/products/type/:type', function (req, res) {
+  if (req.params.type === "hero") {
+    res.json({
+      "count": 2,
+      "results": [
+        {
+          "title": "Batman",
+          "price": "$800/hr",
+          "img_src": "",
+          "type": "hero",
+          "tagline": "NaNaNaNaNaNaNaNaNa",
+          "services": [
+            "detective",
+            "lurking",
+            "gadgets"
+          ]
+        }, {
+          "title": "Spiderman",
+          "price": "$20/hr",
+          "img_src": "",
+          "type": "hero",
+          "tagline": "Friendly Neighborhood Spiderman",
+          "services": [
+            "photography",
+            "climbing",
+            "gadgets"
+          ]
+        }
+      ]
+    })
+  } else {
+    res.json({
+      "count": 2,
+      "results": [
+        {
+          "title": "Megatron",
+          "price": "$50/hr",
+          "img_src": "",
+          "type": "villan",
+          "tagline": "Destruction at a great price",
+          "services": [
+            "demolition",
+            "protection"
+          ]
+        }, {
+          "title": "Dr. Hugo Strange",
+          "price": " $100/hr",
+          "img_src": "",
+          "type": "villan",
+          "tagline": "How...Interesting",
+          "services": [
+            "psychiatrist",
+            "long range planning"
+          ]
+        }
+      ]
+    })
+  }
+})
+
+router.get('/products/name/:title', function (req, res) {
+  if (req.params.title === "Batman") {
+    res.json({
+      "count": 1,
+      "title": "Batman",
+      "price": "$800/hr",
+      "description": "The Dark Knight of Gotham, has a full compliment of technologic tricks and sidekicks to ensure that he can really complete the task you have for him.  Also the worlds greatest detective.",
+      "img_src": "",
+      "bg_img": "",
+      "type": "hero",
+      "tagline": "NaNaNaNaNaNaNaNaNa",
+      "services": [
+        "detective",
+        "lurking",
+        "gadgets"
+      ],
+      "Reviews": [
+        {
+          "username": "penguin",
+          "rating": 0,
+          "review": "Worst hero ever.  Doesn't like birds OR fish"
+        },
+        {
+          "username": "Superman",
+          "rating": 5,
+          "review": "A good friend, and always able to help out when the world is ending"
+        },
+        {
+          "username": "GordonJ",
+          "rating": 5,
+          "review": "Asked him to investigate my wife.  Not only did he find ample evidence of her cheating, he also applied some vengence to thugs down by the river for me!"
+        },
+        {
+          "username": "IronMan",
+          "rating": 2,
+          "review": "Very overpriced. Poor technology, and unwilling to kill his villains."
+        }
+      ]
+    })
+  }
+})
 
 router.get('/products', function (req, res) {
   res.json({
@@ -58,7 +159,6 @@ router.get('/products', function (req, res) {
     ]
   })
 })
-
 
 
 module.exports = router;
